deb1309fba04a5e86dd66a12ab5a58d236464d3c
pianistoff <severyn.h@gmail.com>
2022-07-13 15:07:54 +0300

    Mark up and stylize the button

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React from "react";
 import Die from './components/Die';
 
 export default function App() {
+    const [diceNums, setDiceNums] = React.useState(allNewDice());
+
+    function allNewDice() {
+        function randomNum() {
+            return Math.ceil(Math.random() * 6);
+        }
+        return [randomNum(), randomNum(), randomNum(), randomNum(), randomNum(), randomNum(), randomNum(), randomNum(), randomNum(), randomNum()];
+    }
+
+    function rollDice() {
+        setDiceNums(allNewDice());
+    }
+    
+    const dice = diceNums.map((num, index) => <Die key={index} value={num} />);
     return (
         <main className="main"> 
             <div className="outer-box">
                 <div className="inner-box">
                     <div className="dice">
-                        <Die value={1} />
-                        <Die value={2} />
-                        <Die value={3} />
-                        <Die value={4} />
-                        <Die value={5} />
-                        <Die value={6} />
-                        <Die value={1} />
-                        <Die value={2} />
-                        <Die value={3} />
-                        <Die value={4} />
+                        {dice}
                     </div>
+                    <button onClick={rollDice} className="btn">Roll</button>
                 </div>
             </div>
         </main>
