6d236d3ce74dba33150cee7b9a65549e99ea61da
pianistoff <severyn.h@gmail.com>
2022-07-19 19:39:55 +0300

    Replace numbers with pips on dice and correct their styles

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./die.css"
 
-export default function Die({value, isHeld, hold, id}) {
-    const color = isHeld ? "#59E391" : "white";
-    return (
-    <div className="die-box" onClick={hold} style={{backgroundColor: color}}>
-        <p className="die-num" >{value}</p>
-    </div>
-    )
+const Pip = () => <span className="pip" />;
+
+const Face = ({ children, isHeld, hold }) => <div className={isHeld ? "face held" : "face"} onClick={hold}>{children}</div>;
+
+export default function Die({value, isHeld, hold}) {
+    let pips = Number.isInteger(value)
+		? Array(value)
+				.fill(0)
+				.map((_, i) => <Pip key={i} />)
+		: null;
+	return <Face isHeld={isHeld} hold={hold}>{pips}</Face>;
 }
