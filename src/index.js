ff3a2b9112b176301c75f984722752286d11ee19
pianistoff <94297478+pianistoff@users.noreply.github.com>
2022-09-08 14:21:38 +0300

    Remove font imports to scss

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ require("file-loader?name=[name].[ext]!./index.html");
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "@fontsource/karla/700.css";
-import "@fontsource/inter/400.css";
 import "./styles.scss";
 import { Provider } from "react-redux";
 import store from "./store/storeConfig";
