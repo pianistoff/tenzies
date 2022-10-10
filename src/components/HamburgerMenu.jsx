06755164400deb9e52d6344ded1fc0136345df19
pianistoff <94297478+pianistoff@users.noreply.github.com>
2022-10-10 09:26:53 +0300

    Make hamburger change tableOpen state

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  openTable,
+  closeTable,
+  selectTableOpen,
+} from '../store/tableOpenSlice';
 
 function HamburgerMenu() {
   const [hamburgerActive, setHamburgerActive] = React.useState(false);
+  const tableOpen = useSelector(selectTableOpen);
+  const dispatch = useDispatch();
 
   const handleHamburger = () => {
+    if (tableOpen) {
+      dispatch(closeTable());
+    } else {
+      dispatch(openTable());
+    }
     setHamburgerActive((prevHamburgerActive) => !prevHamburgerActive);
   };
 
